4194451a36e5c3f25397d4c9eea0d2117dd2c81d
Shiz <hi@shiz.me>
2015-05-16 10:01:02 +0200

    Style fix.

--- src/common.h
+++ src/common.h
@@ -1,7 +1,7 @@
-#pragma  once
-#define  DEFAULT_PATH  "/usr/sbin:/usr/bin:/sbin:/bin"
-#define  DEFAULT_TERM  "linux"
-#define  DEFAULT_UMASK 022
-#define  DEFAULT_USER  "root"
+#pragma once
+#define DEFAULT_PATH  "/usr/sbin:/usr/bin:/sbin:/bin"
+#define DEFAULT_TERM  "linux"
+#define DEFAULT_UMASK 022
+#define DEFAULT_USER  "root"
 
 void prepare_env(void);
